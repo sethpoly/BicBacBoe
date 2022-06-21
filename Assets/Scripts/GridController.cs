413b5a60d250381588d4be789b0c57b8d5329645
sethpoly <emailspolyniak@gmail.com>
2022-06-21 09:00:32 -0400

    basic input

--- Assets/Scripts/GridController.cs
+++ Assets/Scripts/GridController.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,19 +8,50 @@ public class GridController : MonoBehaviour
 {
     [SerializeField] private GridManager grid;
 
+    private PlayerControl playerControl;
+
     private float playerInput = 0;
 
     // Current user selection for
     private TileChoice currentSelection = TileChoice.Left;
 
-    private void Update() {
+    void Awake()
+    {
+        playerControl = new PlayerControl();
+    }
+
+    void Start()
+    {
+        playerControl.Player.Enable();
+    }
+
+    private void Update() 
+    {
         PlayerInput();
     }
 
     private void PlayerInput()
     {
-        playerInput = Input.GetAxis("Horizontal");
+        playerInput = playerControl.Player.Move.ReadValue<float>();
 
         // TODO: Update current selection - make maintainable
+        currentSelection = NormalizePlayerInput(playerInput);
+        Debug.Log("Current selection -> " + playerInput);
+    }
+
+    private TileChoice NormalizePlayerInput(float input)
+    {
+        var rawTile = (int)currentSelection + (int)input;
+        var typedTile = (TileChoice)rawTile;
+        List<TileChoice> tiles = Enum.GetValues(typeof(TileChoice)).Cast<TileChoice>().ToList();
+
+        if(tiles.Contains(typedTile)) 
+            return typedTile;
+
+        // Change tile to upper or lower bound
+        if(rawTile < 0) 
+            return tiles.Last();
+         
+        return tiles.First();
     }
 }
